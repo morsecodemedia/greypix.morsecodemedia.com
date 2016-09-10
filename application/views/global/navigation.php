025fd143728ae6a72a3f136926b5c8565806f1de
morsecodemedia <brandon@morsecodemedia.com>
2016-09-09 23:20:03 -0400

    autoloaded URI library, added functionality to determine active states in navigation. Removed Videos from navigation - until that section is built.

--- application/views/global/navigation.php
+++ application/views/global/navigation.php
@@ -5,7 +5,7 @@
     <nav>
         <ul>
             <li><a href="/" <?php echo ($this->uri->segment(1, 0) === 0) ? 'class="act-link"' : ''; ?>>Home</a></li>
-            <li><a href="/albums/" <?php echo ($this->uri->segment(1, 0) == "albums") ? 'class="act-link"' : ''; ?>>Albums</a></li>
+            <li><a href="/albums/" <?php echo ($this->uri->segment(1) === "albums") ? 'class="act-link"' : ''; ?>>Albums</a></li>
             <!-- <li><a href="/videos/">Videos</a></li> -->
         </ul>
     </nav>
