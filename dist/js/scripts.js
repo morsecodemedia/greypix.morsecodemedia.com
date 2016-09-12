cc9299f70c1288024bdc9199569261f16f927e3c
morsecodemedia <brandon@morsecodemedia.com>
2016-09-11 21:57:40 -0400

    debugging import button functionality

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -619,6 +619,10 @@ function importSubmit() {
   $("#import-script").on("click", function() {
     $.ajax({
       url: "https://greypix.morsecodemedia.com/import/import-albums/",
+      dataType: "html",
+      cache: false,
+      crossDomain: true,
+      
       beforeSend: function(){
         $("#import-loader").show();
       },
