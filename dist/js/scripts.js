79b5a0bd161312077f73d0d8fbb443a4c69ac99e
morsecodemedia <brandon@morsecodemedia.com>
2016-09-11 21:44:48 -0400

    debuggin CORS issue on import script

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -619,8 +619,7 @@ function importSubmit() {
   $("#import-script").on("click", function() {
     $.ajax({
       type: "POST",
-      url: "/import/import-albums/",
-      dataType: "jsonp",
+      url: "https://greypix.morsecodemedia.com/import/import-albums/",
       cache: false,
       beforeSend: function(){
         $("#import-loader").show();
