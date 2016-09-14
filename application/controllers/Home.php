25c5b056c237988425d27af76d30daa4d017f72d
morsecodemedia <brandon@morsecodemedia.com>
2016-09-14 17:48:39 -0400

    debugging the distinct query builder

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -29,8 +29,6 @@ class Home extends CI_Controller {
 	  
 	  $randPix = $this->gpdb->getRandomPicturesLimitedBy(12);
     
-    echo "<pre>"; print_r($randPix); echo "</pre>";exit;
-    
     if ($randPix) {
       foreach ($randPix as $rp) {
         
